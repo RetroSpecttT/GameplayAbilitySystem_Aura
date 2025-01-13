9ee81a3a34540101246eedf47fbd345624f74eb7
李悦 <764482442@qq.com>
2025-01-13 23:53:55 +0800

    1

--- Source/Aura/Private/BH/Player/BH_AuraPlayerController.cpp
+++ Source/Aura/Private/BH/Player/BH_AuraPlayerController.cpp
@@ -3,3 +3,28 @@
 
 #include "BH/Player/BH_AuraPlayerController.h"
 
+#include "EnhancedInputSubsystems.h"
+
+ABH_AuraPlayerController::ABH_AuraPlayerController()
+{
+	bReplicates = true;   //确保输入支持多人模式
+}
+
+void ABH_AuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(AuraContext);   //检查一下AuraContext这个输入模组是否有效,无效的话，下面的代码则不执行
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());   //游戏一开始加载这个增强输入模组
+	check(Subsystem);   //检查一下subsystem是否有效，无效的话下面的代码则不执行
+	Subsystem->AddMappingContext(AuraContext,0);   //加在AUraContext这个文件，优先度是最高的0
+
+	bShowMouseCursor = true	;  //显示鼠标指针
+	DefaultMouseCursor = EMouseCursor::Default;   //鼠标指针的风格为Default
+
+	FInputModeGameAndUI InputModeData;   //这个功能使用白字的设定（白字的设定相当于一个struct）
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);   //而白色的设定则把鼠标设置为 不锁定
+	InputModeData.SetHideCursorDuringCapture(false);   //鼠标被屏幕记录到的时候，不要隐藏
+	SetInputMode(InputModeData);   //设置输入模式 为 白字的struct设定
+}
--- Source/Aura/Public/BH/Player/BH_AuraPlayerController.h
+++ Source/Aura/Public/BH/Player/BH_AuraPlayerController.h
@@ -4,7 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "Player/AuraPlayerController.h"
 #include "BH_AuraPlayerController.generated.h"
+
+class UInputComponent;   //提前把这个类放到header里
+
 
 /**
  * 
@@ -13,5 +17,14 @@ UCLASS()
 class AURA_API ABH_AuraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
+public:
+	ABH_AuraPlayerController();   //相当于我们创建了一个函数，函数内容在cpp
+
+protected:
+	virtual void BeginPlay() override;   //开始游戏时执行一次，也相当于一个函数，内容在cpp
+
+private:
+	UPROPERTY(EditAnywhere, Category="Input")   //把这个模组变成可编辑，放到Input分类里
+	TObjectPtr<UInputMappingContext> AuraContext;   //这个输入模组名字叫AuraContext
 };
